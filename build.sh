624a3952803a56fe2d2264e8e2b2d1f2160764cc
Adam Zammit <adam.zammit@acspri.org.au>
2024-07-23 14:12:58 +1000

    use new version

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 #Fail on any error
 set -e
 
-docker buildx build --pull --no-cache --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/mips64le,linux/arm/v7,linux/arm/v6,linux/s390x -t acspri/quexf:1.21.0 -t acspri/quexf:latest -t adamzammit/quexf:1.21.0 -t adamzammit/quexf:latest .
+docker buildx build --pull --no-cache --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/mips64le,linux/arm/v7,linux/arm/v6,linux/s390x -t acspri/quexf:1.21.1 -t acspri/quexf:latest -t adamzammit/quexf:1.21.1 -t adamzammit/quexf:latest .
 
