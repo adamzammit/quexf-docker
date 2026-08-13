f4db6c4e14188939563df19f196be3fa7f49b148
Adam Zammit <adam.zammit@acspri.org.au>
2026-08-13 13:27:32 +1000

    fix syntax in Dockerfile and update build

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 #Fail on any error
 set -e
 
-docker buildx build --pull --no-cache --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/mips64le,linux/arm/v7,linux/arm/v6,linux/s390x -t acspri/quexf:1.22.0 -t acspri/quexf:latest -t adamzammit/quexf:1.22.0 -t adamzammit/quexf:latest .
 
+sysctl -w kernel.randomize_va_space=0
+docker buildx build --pull --no-cache --push --platform linux/amd64,linux/arm64,linux/ppc64le,linux/arm/v7,linux/arm/v6,linux/s390x -t acspri/quexf:1.22.0 -t acspri/quexf:latest -t adamzammit/quexf:1.22.0 -t adamzammit/quexf:latest .
+sysctl -w kernel.randomize_va_space=2
